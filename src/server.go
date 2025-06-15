8b8986641472873a57cefa7eaacfbec81b51bfb9
Dhruv <62135445+dhr412@users.noreply.github.com>
2025-06-15 22:46:49 +0530

    Add IP address control flag

--- src/server.go
+++ src/server.go
@@ -29,13 +29,14 @@ func triggerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	serveIp := flag.String("ip", "0.0.0.0", "IP address to bind the server to")
 	port := flag.Int("port", 3200, "Port to run the server on")
 	flag.Parse()
 
 	http.HandleFunc("/triggered", triggerHandler)
 
-	addr := fmt.Sprintf(":%d", *port)
-	fmt.Println("Server running on http://0.0.0.0" + addr)
+	addr := fmt.Sprintf("%s:%d", *serveIp, *port)
+	fmt.Printf("Server running on http://%s:%d\n", *ip, *port)
 
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("Server failed: %v", err)
