ee76b425641799aff5d612fac930669acb1050bb
Dhruv <62135445+dhr412@users.noreply.github.com>
2025-06-15 17:33:54 +0530

    Refactor key automation logic

--- src/autom.cpp
+++ src/autom.cpp
@@ -3,23 +3,17 @@
 int main() {
     INPUT keys[4] = {};
 
-    // Press ALT
-    keys[0].type = INPUT_KEYBOARD;
-    keys[0].ki.wVk = VK_MENU;
+    const WORD vKeys[] = {VK_MENU, VK_ESCAPE};
+    for (int i = 0; i < 2; ++i) {
+        // Press key
+        keys[i * 2].type = INPUT_KEYBOARD;
+        keys[i * 2].ki.wVk = vKeys[i];
 
-    // Press ESC
-    keys[1].type = INPUT_KEYBOARD;
-    keys[1].ki.wVk = VK_ESCAPE;
-
-    // Release ESC
-    keys[2].type = INPUT_KEYBOARD;
-    keys[2].ki.wVk = VK_ESCAPE;
-    keys[2].ki.dwFlags = KEYEVENTF_KEYUP;
-
-    // Release ALT
-    keys[3].type = INPUT_KEYBOARD;
-    keys[3].ki.wVk = VK_MENU;
-    keys[3].ki.dwFlags = KEYEVENTF_KEYUP;
+        // Release key
+        keys[i * 2 + 1].type = INPUT_KEYBOARD;
+        keys[i * 2 + 1].ki.wVk = vKeys[i];
+        keys[i * 2 + 1].ki.dwFlags = KEYEVENTF_KEYUP;
+    }
 
     UINT sent = SendInput(4, keys, sizeof(INPUT));
 
